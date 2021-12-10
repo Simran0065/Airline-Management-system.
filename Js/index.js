3a043611df07e97417cb59cc6464c73bb4a26c61
Simran0065 <77879909+Simran0065@users.noreply.github.com>
2021-12-10 16:59:03 +0530

    Add files via upload

---
+++ Js/index.js
@@ -0,0 +1,78 @@
+// image carousel conroller
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+function showSlidesauto() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlidesauto, 15000); // Change image every 2 seconds
+}
+
+window.addEventListener("load", showSlidesauto);
+
+const exchange  = document.getElementById("exchange");
+const departure = document.getElementById("from");
+const arrival = document.getElementById("to");
+
+exchange.onclick = function(){
+    let temp = departure.value;
+    departure.value = arrival.value;
+    arrival.value = temp;
+};
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+   dd = '0' + dd;
+}
+
+if (mm < 10) {
+   mm = '0' + mm;
+} 
+    
+today = yyyy + '-' + mm + '-' + dd;
+document.getElementById("depart-date").setAttribute("min", today);
+document.getElementById("return-date").setAttribute("min", today);
+document.getElementById("return-date").value = today;
